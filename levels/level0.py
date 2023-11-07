135d8b64d67a1cacf7f74cadf78bae5dcb36eb2b
Ryan McMillan <Ryan.j.mcmillan34@gmail.com>
2023-11-07 19:27:26 +1100

    testing transition to level scripts.

--- levels/level0.py
+++ levels/level0.py
@@ -5,7 +5,8 @@ import hashlib
 LEVEL = 0
 
 def main():
-    pass
+    # Create bandito0 user and set password to bandito0
+    os.system("adduser -h '/home/bandito0' -s /bin/bash -D 'bandito0' && echo 'bandito0:bandito0' | chpasswd" )
 
 if __name__ == '__main__':
     main()
