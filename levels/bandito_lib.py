913de4584d80629b41247918ef06b1c50a7ae4e5
Ryan McMillan <Ryan.j.mcmillan34@gmail.com>
2023-11-07 19:55:23 +1100

    Test update sshd_config

--- levels/bandito_lib.py
+++ levels/bandito_lib.py
@@ -1,8 +1,15 @@
 # Library to group together common functions
+import os
 import hashlib
 from datetime import datetime
 
 
 def get_password_hash() -> str:
     '''Return SHA256 string of current datetime YYYYMMDDHHMMSSmmm'''
-    return hashlib.sha256(bytes(datetime.utcnow().strftime('%Y%m%d%H%M%s%f'), 'utf-8')).hexdigest()
+    return hashlib.sha256(bytes(datetime.utcnow().strftime('%Y%m%d%H%M%s%f'),
+                                'utf-8')).hexdigest()
+
+
+def ssh_access(level):
+    '''Add user to the allowed list of SSH users'''
+    os.system(f"sed -i '/^AllowUsers/ s/$/ bandito{level}/' /etc/sshd_config")
--- levels/level0.py
+++ levels/level0.py
@@ -1,12 +1,18 @@
 # Initial setup script for bandito0 user
 import os
 import hashlib
+import bandito_lib
 
 LEVEL = 0
 
 def main():
     # Create bandito0 user and set password to bandito0
     os.system("adduser -h '/home/bandito0' -s /bin/bash -D 'bandito0' && echo 'bandito0:bandito0' | chpasswd" )
+
+    #Allow SSH access
+    bandito_lib.add_ssh(LEVEL)
+
+    os.system("cat /etc/ssh/sshd_config")
 
 if __name__ == '__main__':
     main()
