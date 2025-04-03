5be2880fac6a57fc7f60592f1a61f8c26332aac7
darius <dariusehoubo@gmail.com>
2025-04-04 01:35:15 +0200

    Co-authored-by: Loïc Harry LEENAERTS <THE07s@users.noreply.github.com>

--- levels/level3.py
+++ levels/level3.py
@@ -2,18 +2,27 @@
 
 import os
 import bandito_lib
+import level4
 
 def main():
+
     # Set constant for level
     LEVEL = 3
 
     # Add bandito3 user
     password = bandito_lib.add_user(LEVEL)
 
+    # Configure the level, special character filename
+    os.system("touch /home/bandito2/-")
+    os.system(f"echo '{password}' >> /home/bandito2/-")
+    os.system("chown bandito3:bandito2 /home/bandito2/-")
+    os.system("chmod 640 /home/bandito2/-")
     
-
     # Restrict home folder access
     bandito_lib.read_only_home_folder(LEVEL)
+
+    #Configure level3-4
+    level4.main()
 
 if __name__ == '__main__':
     main()
