e524ddc40d3ce03e04d63905b67c0386c53eff6b
Loïc Harry LEENAERTS <loic_leenaerts@reseau.eseo.fr>
2025-04-05 03:30:06 +0200

    Mise à jour de la configuration SSH pour corriger la redirection du fichier .bash_profile et ajout de la redirection de l'historique Bash dans les fichiers de configuration.

--- niveaux/CTF_lib.py
+++ niveaux/CTF_lib.py
@@ -40,8 +40,7 @@ def config_ssh(niv: int):
     os.system(f"sed -i '/^AllowUsers/ s/$/ niveau{niv}/' /etc/ssh/sshd_config")
     os.system(f"mkdir -p /home/niveau{niv}/.ssh/")
     os.system(f"export PS1='\033[32m\033[40m\\u@\\h$; clear' > /home/niveau{niv}/.ssh/rc")
-    os.system(f"echo 'source /home/niveau{niv}/.bash_profile' > /home/niveau{niv}/.ssh/rc")
-
+    os.system(f"echo '. /home/niveau{niv}/.bash_profile' > /home/niveau{niv}/.ssh/rc")
 
 def ecrire_fichier_mdp(niv: int, mdp: str):
     '''Écrit le mot de passe dans /etc/niveau_mdps/niveauX et restreint les permissions'''
@@ -55,10 +54,12 @@ def ecrire_fichier_mdp(niv: int, mdp: str):
 def config_bash(niv: int):
     '''Configure les profils Bash (.bashrc et .bash_profile) pour l'utilisateur'''
     ps1 = "export PS1='\\[\\033[40m\\]\\[\\033[01;32m\\]\\u@\\h:\\[\\033[01;34m\\]\\w$ \\[\\033[40m\\]\\[\\033[00;32m\\]'"
+    # Redirection de l'historique dans un fichier en /tmp
+    hist = "export HISTFILE=/tmp/$(whoami)_bash_history"
     os.system(f"touch /home/niveau{niv}/.bashrc")
-    os.system(f'echo "{ps1}" > /home/niveau{niv}/.bashrc; source /home/niveau{niv}/.bashrc')
+    os.system(f'echo "{ps1}" > /home/niveau{niv}/.bashrc; echo "{hist}" >> /home/niveau{niv}/.bashrc; source /home/niveau{niv}/.bashrc')
     os.system(f"touch /home/niveau{niv}/.bash_profile")
-    os.system(f'echo "{ps1}" >> /home/niveau{niv}/.bash_profile; source /home/niveau{niv}/.bash_profile')
+    os.system(f'echo "{ps1}" >> /home/niveau{niv}/.bash_profile; echo "{hist}" >> /home/niveau{niv}/.bash_profile; source /home/niveau{niv}/.bash_profile')
     os.system(f'echo "alias ls=\'ls --color=never\'" >> /home/niveau{niv}/.bash_profile; source /home/niveau{niv}/.bash_profile')
 
 
