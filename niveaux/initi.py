da97b360d837d5af3b5bef413f75ca459a9db4a7
Loïc Harry LEENAERTS <loic_leenaerts@reseau.eseo.fr>
2025-04-06 16:15:29 +0200

    Ajout d'une attente après la création des utilisateurs pour éviter les erreurs de chown/open, et mise à jour du message de succès.

--- niveaux/initi.py
+++ niveaux/initi.py
@@ -1,16 +1,21 @@
 # Script d'initialisation globale de tous les utilisateurs de niveau de CTF Hackaton
 import CTF_lib
 import niveau0
+import time
 
 def main():
     print("Initialisation des utilisateurs de niveau de CTF Hackaton…")
-    
+
     for niveau in range(0, 25):
         print(f" → Création de l'utilisateur niveau{niveau}")
         CTF_lib.ajout_utilisateur(niveau)
 
-    print("\n✅ Tous les utilisateurs ont été créés avec succès.\nTu peux maintenant lancer le script niveau0.py pour démarrer le jeu.")
+    # Attente pour éviter les erreurs de chown/open trop précoces
+    print("⏳ Attente que les utilisateurs et répertoires soient bien enregistrés...")
+    time.sleep(2)
 
+    print("\n✅ Tous les utilisateurs ont été créés avec succès.\nTu peux maintenant lancer le script niveau 0.")
+    
     # Lancement du niveau 0
     print("Génération du niveau 0...")
     niveau0.main()
