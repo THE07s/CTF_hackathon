5454305f329686e92d1fb3e40338b0e03873da21
Loïc Harry LEENAERTS <loic_leenaerts@reseau.eseo.fr>
2025-04-10 09:45:03 +0200

    Mise à jour des instructions dans banners.sh pour clarifier les noms d'utilisateur et les emplacements des mots de passe. Amélioration de la formulation pour une meilleure compréhension des wargames.

--- scripts_bash/banners.sh
+++ scripts_bash/banners.sh
@@ -13,18 +13,18 @@ echo -e "\033[40m\033[31m
 ░        ░  ░░ ░ ░   ▒    ░ ░    ░ ░     ░     ░   ░ ░░ ░   ░   ░      ░         ░      ░ ░    
 ░ ░      ░  ░  ░     ░  ░   ░  ░   ░  ░  ░  ░        ░      ░   ░  ░   ░ ░                     ░
 ░                                                                      ░                       \033[0m
-Fait partie du projet Portable Wargame NUC (PWN) — série de challenges CTF"
+"
 
 echo "--[ Jouer au jeu ]--
 
-Cette machine peut contenir plusieurs wargames.
 Si vous jouez à \"somegame\", alors:
 
-    * Les noms d'utilisateur sont somegame0, somegame1, ...
-    * La plupart des niveaux se trouvent dans /somegame/.
-    * Les mots de passe pour chaque niveau sont stockés dans /etc/somegame_pass/.
+    * Les noms d'utilisateur sont niveau0, niveau1, ...
+    * La plupart des niveaux se trouvent dans le /home de chaque niveau.
+    * Les mots de passe pour chaque niveau sont stockés dans /etc/niveau_mdps/.
 
-L'accès en écriture aux répertoires personnels étant désactivé, il est conseillé de créer un répertoire de travail avec un nom difficile à deviner dans /tmp/. Vous pouvez utiliser la commande \"mktemp -d\" afin de générer un répertoire aléatoire et difficile à deviner dans /tmp/. L'accès en lecture à /tmp/ est désactivé et l'accès à /proc est restreint pour empêcher les utilisateurs de se surveiller mutuellement. Les fichiers et répertoires aux noms facilement devinables ou trop courts seront supprimés périodiquement ! Le répertoire /tmp est régulièrement effacé.
+L'accès en écriture aux répertoires personnels étant désactivé, il est conseillé de créer un répertoire de travail avec un nom difficile à deviner dans /tmp/. Vous pouvez utiliser la commande \"mktemp -d\" afin de générer un répertoire aléatoire et difficile à deviner dans /tmp/. L'accès à /proc est restreint pour empêcher les utilisateurs de se surveiller mutuellement. Les fichiers et répertoires aux noms facilement devinables ou trop courts seront supprimés périodiquement ! Le répertoire /tmp est régulièrement effacé.
+
 S'il vous plaît, soyez respectueux :
 
     * Ne laissez pas de processus orphelins en cours d'exécution
@@ -33,9 +33,9 @@ S'il vous plaît, soyez respectueux :
 
 --[ Plus d'informations ]--
 
-Pour plus d'informations concernant les wargames individuelles, visitez
+Pour plus d'informations, visitez
 https://github.com/THE07s/CTF_hackathon
 
 Si vous trouvez un bug, n'hésitez pas à signaler un problème sur le Github ci-dessus.
 
-Profitez de votre séjour!"
+Bonne chance!"
